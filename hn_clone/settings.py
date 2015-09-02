db2be0140d71411ac8bca5cebab91981701bcbff
stummjr <stummjr@gmail.com>
2015-09-02 18:47:24 -0300

    settings for heroku

--- hn_clone/settings.py
+++ hn_clone/settings.py
@@ -20,9 +20,8 @@ BASE_DIR = Path(__file__).parent
 
 # Celery settings
 BROKER_URL = 'django://'
-CELERY_ACCEPT_CONTENT = ['json']
+CELERY_ACCEPT_CONTENT = ['json', 'pickle']
 CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
 CELERY_RESULT_BACKEND = 'djcelery.backends.database:DatabaseBackend'
 CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
 
