8115cdbc2faee6e07e43cece54eca2f53aaf897a
stummjr <stummjr@gmail.com>
2015-09-05 16:36:09 -0300

    updated requirements

--- hn_clone/settings.py
+++ hn_clone/settings.py
@@ -29,7 +29,7 @@ CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
 CELERYBEAT_SCHEDULE = {
     'update_events': {
         'task': 'webapp.tasks.crawl',
-        'schedule': timedelta(minutes=15)
+        'schedule': timedelta(minutes=30)
     }
 }
 
@@ -42,7 +42,6 @@ ALLOWED_HOSTS = [".herokuapp.com"]
 
 
 # Application definition
-
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
